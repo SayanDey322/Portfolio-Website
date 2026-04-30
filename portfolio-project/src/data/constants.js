4a9bf4e96df66a2192db5c7ccebf4d132230458e
Sayon Dey <deysayon6@gmail.com>
2026-04-30 13:35:36 +0530

    Updated

--- portfolio-project/src/data/constants.js
+++ portfolio-project/src/data/constants.js
@@ -7,7 +7,7 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/SayanDey322",
+  github: "https://github.com/SayanDey322", 
   resume:
     "https://drive.google.com/file/d/1dkuIbJeucuT2wMQ6VNko2ak8hepSfPWP/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/sayon-dey-3a0876324/",
